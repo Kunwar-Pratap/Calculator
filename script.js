7ce04307142598176cec811a3f3a85264f1ba03b
Kunwar Pratap <kunwarkudos@gmail.com>
2023-02-12 17:14:17 +0530

    initial commit

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let clearScr = document.querySelector('#clearScr');
 let backSpace = document.querySelector('#backSpace');
 let ansEvaluate = document.querySelector('#ansEvaluate');
 let buttons = document.querySelectorAll('button');
-let funcBtn = document.querySelector('.func-btn');
 
 let answerEvaluate = [];
 
