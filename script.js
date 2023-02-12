6c2dd8e33738be7a6c3c09a1aa0a2cde42c9d972
Kunwar Pratap <kunwarkudos@gmail.com>
2023-02-12 17:04:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let userInput = document.querySelector('.user-input');
+let ansScr = document.querySelector('.ans-scr');
+let clearScr = document.querySelector('#clearScr');
+let backSpace = document.querySelector('#backSpace');
+let ansEvaluate = document.querySelector('#ansEvaluate');
+let buttons = document.querySelectorAll('button');
+let funcBtn = document.querySelector('.func-btn');
+
+let answerEvaluate = [];
+
+clearScr.addEventListener('click', () => {
+    answerEvaluate = [''];
+    ansScr.innerHTML = 0;
+    userInput.className = 'user-input';
+    ansScr.className = 'ans-scr';
+    document.reload;
+    userInput.style.display = 'block';
+});
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (!button.id.match("backSpace")) {
+            answerEvaluate.push(button.value);
+            userInput.innerHTML = answerEvaluate.join("");
+        }
+        if (button.classList.contains('num-btn')) {
+            ansScr.innerHTML = eval(answerEvaluate.join(""));
+        }
+        if (button.id.match('backSpace')) {
+            answerEvaluate.pop();
+            userInput.innerHTML = answerEvaluate.join("");
+            ansScr.innerHTML = eval(answerEvaluate.join(""));
+        }
+        if (button.id.match('ansEvaluate')) {
+            userInput.className = 'ansScr';
+            ansScr.className = 'userInput';
+            ansScr.style.color = '#16FF00';
+            ansScr.style.fontSize = '20px';
+            userInput.style.display = 'none';
+        }
+        if (typeof eval(answerEvaluate.join("")) === 'undefined') {
+            ansScr.innerHTML = 0;
+        }
+    });
+});
